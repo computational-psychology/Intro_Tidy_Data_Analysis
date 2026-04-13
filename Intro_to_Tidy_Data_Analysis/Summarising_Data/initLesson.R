ab906885c70ffc5d9696de2039a7e9630fc08024
Joris Vincent <joris.vincent@tu-berlin.de>
2026-04-13 11:19:10 +0200

    feat: third lesson, on summarizing data

---
+++ Intro_to_Tidy_Data_Analysis/Summarising_Data/initLesson.R
@@ -0,0 +1,8 @@
+# Code placed in this file will be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+# Silently load the tidyverse (students should already have it from lesson 2,
+# but load it here in case they restart R between lessons)
+suppressPackageStartupMessages(library(tidyverse))
